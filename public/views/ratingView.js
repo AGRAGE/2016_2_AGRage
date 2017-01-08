62e8b7bd9cc8a5779fa700adfdfb332ff6ffb13f
Sany <you@example.com>
2017-01-08 22:24:10 +0300

    rating

--- public/views/ratingView.js
+++ public/views/ratingView.js
@@ -17,10 +17,7 @@
             .then((responseObj) => {
                 console.log(responseObj);
 
-                responseObj.forEach(user => {
-                   // this._el.innerHTML = '<div> user.getLogin() <br> </div>';
-                   console.log(user.username);
-                })
+               
 
             })
             .catch((err) => {
@@ -46,6 +43,36 @@
             if (this.backGround[0]) {
                 this.backGround[0].hidden = "hidden";
            }
+        }
+
+
+        table(){
+            var html = '<table>';   
+
+            responseObj.forEach(user => {
+                       // this._el.innerHTML = '<div> user.getLogin() <br> </div>';
+                console.log(user.username);
+                html += '<tr>';
+                html += '<td>'+user.username+'</td>' + '<td>'+user.rating+'</td>';
+                html += '</tr>';
+            })
+            html += '<td></td><td></td></tr>';
+            html +='</table>';
+            // for(var i in ) {
+            //     if(cnt == 0) {
+            //         html += '<tr>';
+            //     }
+            //     html += '<td>'+products[i]['id']+'</td>' + '<td>'+products[i]['name']+'</td>';
+            //     cnt++;
+            //     if(cnt == 2) {
+            //         cnt = 0;
+            //         html += '</tr>';
+            //     }
+            // } 
+            // if(cnt != 0) {
+            //     html += '<td></td><td></td></tr>';
+            // }
+            // html +='</table>';
         }
     }
 
