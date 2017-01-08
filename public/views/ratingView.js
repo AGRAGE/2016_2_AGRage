574fa8f643ecbd0de00325c2e0ea1a8c90a64293
Sany <you@example.com>
2017-01-08 22:14:59 +0300

    rating

--- public/views/ratingView.js
+++ public/views/ratingView.js
@@ -19,7 +19,7 @@
 
                 responseObj.forEach(user => {
                    // this._el.innerHTML = '<div> user.getLogin() <br> </div>';
-                   console.log(window.myUser.getLogin());
+                   console.log(user.login);
                 })
 
             })
