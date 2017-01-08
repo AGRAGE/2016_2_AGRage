26e44aacb061a69c6c344d2e5ca0bc76ecef7ea1
Sany <you@example.com>
2017-01-09 00:08:52 +0300

    rating

--- public/views/ratingView.js
+++ public/views/ratingView.js
@@ -46,7 +46,7 @@
 
 
 		table(responseObj) {
-			var str = "<table border=\"1\" cellspacing=\"0\" cellpadding=\"2\" align=\"center\" width = \"2000px\"  color = \"#8B0000\">";
+			var str = "<table class = \"table\" >";
 			responseObj.forEach(user => {
 				str+="<tr>";
 				str+="<td>" + user.username + "</td>";
