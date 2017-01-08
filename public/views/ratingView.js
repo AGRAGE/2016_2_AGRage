45600ba34d512756054824e6f23474edf3cd72e3
Your Name <andreykuleb@mail.ru>
2017-01-08 23:14:28 +0300

    resolve merge conflict 2

--- public/views/ratingView.js
+++ public/views/ratingView.js
@@ -3,19 +3,18 @@
 	const Button = window.Button;
 	const View = window.View;
 
-
-    class ratingView extends View {
-        constructor(options = {}){
-            super(options);
-            this._el = document.querySelector('.js-rating');
-            this.backGround = document.getElementsByClassName('bg');
-            this.backGround[0].hidden = "";
+	class ratingView extends View {
+		constructor(options = {}) {
+			super(options);
+			this._el = document.querySelector('.js-rating');
+			this._el.hidden="";
 			window.myUser = new User();
 
 			window.myUser.rating()
 				.then((responseObj) => {
 					console.log(responseObj);
 					this.table(responseObj);
+
 
 				})
 				.catch((err) => {
@@ -56,88 +55,8 @@
 			})
 			str += "</table>";
 			this._el.innerHTML = str;
-			/*
-			document.write("<table border=\"1\" cellspacing=\"0\" cellpadding=\"2\" align=\"center\">")
-			responseObj.forEach(user => {
-				document.write("<tr>");
-				document.write("<td>" + user.username + "</td>")
-				document.write("<td>" + user.rating + "</td>")
-				document.write("</tr>")
-			})
-			document.write("</table>")
-			*/
 		}
 	}
 
 	window.ratingView = ratingView;
-=======
-
-           //console.log();
-			//this._el.innerHTML = '<div> this.sender.getLogin() </div>';
-
-            this._el.innerHTML = '<div> fsdgfhghfdg</div>';
-
-
-        }
-
-         resume() {
-            super.resume();
-
-            if (this.backGround[0]) {
-                this.backGround[0].hidden = "";
-            }
-
-        }
-
-        pause() {
-           super.pause();
-
-            if (this.backGround[0]) {
-                this.backGround[0].hidden = "hidden";
-           }
-        }
-
-
-        table(responseObj){
-
-            // var html = '<table>';
-
-            // responseObj.forEach(user => {
-            //            // this._el.innerHTML = '<div> user.getLogin() <br> </div>';
-            //     console.log(user.username);
-            //     html += '<tr>';
-            //     html += '<td>'+user.username+'</td>' + '<td>'+user.rating+'</td>';
-            //     html += '</tr>';
-            // })
-            // html += '<td></td><td></td></tr>';
-            // html +='</table>';
-            // var i, j;
-        // Сначала нарисуем саму таблицу.
-        // Внутренние кавычки (внутри тэгов) можно сделать одиночными,
-        // а можно, как здесь, воспользоваться спецсимволом (см. урок 2)
-        document.write("<table border=\"1\" cellspacing=\"0\" cellpadding=\"2\" align=\"center\">")
-
-        responseObj.forEach(user => {
-            document.write("<tr>");
-            document.write("<td>" + user.username + "</td>")
-            document.write("<td>" + user.rating + "</td>")
-            document.write("</tr>")
-        })
-        document.write("</table>")
-
-
-
-        // for (i = 2; i <= 10; i++)
-        // {document.write("<tr>");
-        //     for (j = 2; j < 10; j++)
-        //     {document.write("<td>" + "</td>")}
-        // document.write("</tr>")
-        // }
-        // document.write("</table>")
-        // }
-        }
-    }
-
-    window.ratingView = ratingView;
->>>>>>> 23081facfa92bded6ea719e7c46025bbeefdc38f
 }());
