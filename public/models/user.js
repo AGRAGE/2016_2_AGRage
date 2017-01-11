7dce2169aafcd06e22172c9ff983f2ada50eff97
Your Name <andreykuleb@mail.ru>
2017-01-11 05:28:04 +0300

    parseJSON 2

--- public/models/user.js
+++ public/models/user.js
@@ -88,7 +88,7 @@
 							//resolve(response.json());
 						}
 					)
-					.then(this._parseJSON)
+					.then(resolve(this._parseJSON))
 					.catch(function(err) {
 						console.log('Fetch Error :-S', err);
 						let responseObj = {
